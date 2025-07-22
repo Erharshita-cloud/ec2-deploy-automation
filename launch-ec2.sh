3ac69747e54ab8a9aff1d2ecd6764d60285c42d0
Rechi <Rechi@gmail.com>
2025-07-22 20:16:56 +0530

    Second Commit

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -30,17 +30,4 @@ PUBLIC_IP=$(aws ec2 describe-instances \
 
 echo "Public IP: $PUBLIC_IP"
 
-scp -i "$PEM_FILE" deploy_app.sh ec2-user@$PUBLIC_IP:/home/ec2-user/
-ssh -i "$PEM_FILE" ec2-user@$PUBLIC_IP "bash deploy_app.sh"
-
-if curl -s --head http://$PUBLIC_IP | grep "200 OK" > /dev/null; then
-  echo "App is running!"
-else
-  echo "App is NOT reachable"
-fi
-
-echo "Waiting 1 minute before stopping..."
-sleep 60
-aws ec2 stop-instances --instance-ids $INSTANCE_ID
-
 
